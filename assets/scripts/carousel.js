444da631396eca3ed3305b22fba5af3c190d6e86
Witor Linhares <witorlinhares@gmail.com>
2024-08-23 01:20:54 -0300

    carousel

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 3000, //time between one statement and another
+      disableOnInteraction: false,
+    },
+
+    speed: 1000, //carousel transition speed
+
+  });
